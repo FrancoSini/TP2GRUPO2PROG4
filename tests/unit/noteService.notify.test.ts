6cf074546bb39f3261ee1fe073719228475be310
BarreraMateo <mateeoo2003@icloud.com>
2026-09-23 20:09:29 -0300

    Agrego importaciones necesarias

---
+++ tests/unit/noteService.notify.test.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { NoteServiceImpl } from '../../src/services/NoteService';
+import { SqliteNoteRepository } from '../../src/repositories/NoteRepository';
+import { createDb } from '../../src/db/connection';
+import { notify } from '../../src/services/notificationService';
