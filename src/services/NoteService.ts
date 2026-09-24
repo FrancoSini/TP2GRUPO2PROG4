1164ba28cec5e744825d77dcb101e18a28effb7e
FrancoSini <francosinigaglia2003@gmail.com>
2026-09-24 10:18:13 -0300

    test: Ejercicio 3 (rojo - tests de getNote)

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -43,8 +43,8 @@ export class NoteServiceImpl implements NoteService {
   getNote(id: number): Note | undefined {
     // 🔴🟢 EJERCICIO 3: ciclo completo (test + implementación).
     
-    return this.repo.findById(id);
-    
+    //return this.repo.findById(id);
+    throw new Error('getNote: no implementado (Ejercicio 3)');
   }
 
   updateNote(id: number, patch: NotePatch): Note | undefined {
